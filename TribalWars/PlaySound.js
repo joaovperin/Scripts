84d4925737cbf8c61e6b2f28ab6823d871a1421d
Perin <perin@rech.com.br>
2017-12-07 13:01:41 -0200

    Added exemplo promise

--- TribalWars/PlaySound.js
+++ TribalWars/PlaySound.js
@@ -1,5 +1,11 @@
-(function(a){
-   a = $('body').append('<audio src="http://www.soundjay.com/button/beep-07.wav" /></audio>').find('audio')[0];
-   a.play();
+(function (a) {
+    a = $('body').append('<audio src="http://www.soundjay.com/button/beep-07.wav" /></audio>').find('audio')[0];
+    a.play();
+    /*
+        var promise = $.Deferred();
+        return promise.promise();
+        dialog.promise.resolve();
+        dialog.promise.reject(err);
+    */
 })();
 
