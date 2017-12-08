f88314004789a3057fc1cb10670eb7a9359dd7bb
Perin <perin@rech.com.br>
2017-12-08 15:34:13 -0200

    added spring boot example tst

---
+++ WebServer/WebServer/src/main/java/br/jpe/core/WebServer.java
@@ -0,0 +1,17 @@
+package br.jpe.core;
+
+//import org.springframework.boot.*;
+//import org.springframework.boot.autoconfigure.*;
+//import org.springframework.stereotype.*;
+//import org.springframework.web.bind.annotation.*;
+
+public class WebServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("e");
+//        SpringApplication.run(WebServer.class, args);
+    }
+}
